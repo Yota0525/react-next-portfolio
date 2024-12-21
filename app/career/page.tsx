66ac2faa5fbd33ea5aa686ed8d9693fb1195f522
Yota0525 <KTC24A31G0013@edu.kyoto-tech.ac.jp>
2024-12-21 10:18:21 +0900

    ニュースにISRを適用

--- app/career/page.tsx
+++ app/career/page.tsx
@@ -4,7 +4,7 @@ import Pagination from "@/app/_components/Pagination";
 import SearchField from "@/app/_components/SearchField";
 import { CAREER_LIST_LIMIT } from "@/app/_constants";
 
-export const revalidate = 0;
+export const revalidate = 60;
 
 export default async function Page() {
     
